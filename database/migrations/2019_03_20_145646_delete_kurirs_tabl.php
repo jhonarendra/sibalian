f61d2d5c7a678b1e3ad0e21ad4d311154e88cb35
gunawan26 <33386634+gunawan26@users.noreply.github.com>
2019-03-20 23:16:07 +0800

    migration

---
+++ database/migrations/2019_03_20_145646_delete_kurirs_tabl.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteKurirsTabl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('order_obats', function (Blueprint $table) {
+            // $table->dropForeign(['id_kurir']);
+            if (Schema::hasColumn('order_obats','id_kurir')) {
+                //
+               
+                $table->dropColumn('id_kurir');
+
+                
+            }
+            if (Schema::hasColumn('order_obats','id_dokter')) {
+                //
+              
+                $table->dropForeign(['id_dokter']);
+                $table->dropColumn('id_dokter');
+
+                
+            }
+            if (Schema::hasColumn('order_obats','resep_dokter')) {
+             
+                $table->dropColumn('resep_dokter');
+
+                
+            }
+            if (Schema::hasColumn('order_obats','bukti_pembayaran')) {
+                //
+
+                $table->dropColumn('bukti_pembayaran');
+                
+            }
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+    }
+}
